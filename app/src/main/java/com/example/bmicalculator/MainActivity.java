aa322a1d61ac75dda476720b19d3bdc17dd08f8c
darshan-1204 <dabhidarshan2004@gmail.com>
2022-12-13 17:08:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText height,weight;
+    Button btn;
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        height = findViewById(R.id.height);
+        weight = findViewById(R.id.weight);
+        btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                float h = Float.parseFloat(height.getText().toString());
+                float w = Float.parseFloat(weight.getText().toString());
+
+                float meter= h/100;
+
+                float ans = w/(meter*meter);
+
+                tv.setText("BMI ="+ans);
+            }
+        });
+    }
+}
